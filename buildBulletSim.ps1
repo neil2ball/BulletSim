c6dd28b934f844cc5cc5012ad302ad742e6c3785
Robert Adams <misterblue@misterblue.com>
2023-12-07 16:26:56 +0000

    change buildBulletSim.ps1 to try and pass pre-processor variables correctly

--- buildBulletSim.ps1
+++ buildBulletSim.ps1
@@ -2,4 +2,6 @@
 $BULLETVERSION = Get-Content -Path .\lib\VERSION
 $BULLETSIMVERSION = Get-Content -Path .\VERSION
 
-msbuild -p:BULLETVERSION=$BULLETVERSION -p:BULLETSIMVERSION=$BULLETSIMVERSION -p:Configuration=Release BulletSim.sln
+Set-Item -Path Env:CL -Value "/DBULLETVERSION#$BULLETVERSION /DBULLETSIMVERSION#$BULLETSIMVERSION"
+
+msbuild -p:Configuration=Release BulletSim.sln
